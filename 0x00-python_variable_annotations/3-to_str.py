9142cc1d2bdd1294a0304d6144c6a37e382fe21a
Donald Brooks <brooksd079@gmail.com>
2023-04-27 20:14:25 +0300

    string representation of floats

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def to_str(n: float) -> str:
+    """
+    A method that takes a float argument
+    and returns a string.
+    Returns:
+        The string representation of the float.
+    """
+    return str(n)
