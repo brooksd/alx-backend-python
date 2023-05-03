bcb51eb548107c6c3c7df3acfec3114c7f54249e
Donald Brooks <brooksd079@gmail.com>
2023-05-03 12:36:44 +0300

    coroutines test fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+""" The basics of async """
 
+import asyncio
 from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
@@ -7,15 +9,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    asynchronous coroutine that takes in two
-    arguments and returns a list
-    of all the delays. The number of delays
-    is the same as n. The list should be in ascending order
-    without using sort().
+    spawn wait_random n times with the specified max_delay.
     """
-    wait_list = []
-
-    for i in range(n):
-        wait_list.append(await wait_random(max_delay))
-
-    return sorted(wait_list)
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
